61e50702ff45eac0c88bc316c9ecc1d3c50bc8f3
dmitrygortex <gortex@internet.ru>
2025-02-01 22:30:14 +0500

    feat: add some GET requests

--- GameShopCRUD/Program.cs
+++ GameShopCRUD/Program.cs
@@ -1,3 +1,5 @@
+using GameShopCRUD.Dtos;
+
 namespace GameShopCRUD;
 
 public class Program
@@ -7,6 +9,39 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
+        List<GameDto> games =
+        [
+            new GameDto(
+                1,
+                "Watch Dogs 2",
+                "HackGame",
+                4200M,
+                new DateOnly(2018, 4, 3)
+            ),
+            
+            new GameDto(
+                2,
+                "Rayman",
+                "Platformer",
+                3100M,
+                new DateOnly(2016, 1, 24)
+                ),
+            
+            new GameDto(
+                3,
+                "Minecraft",
+                "Survive game",
+            1200,
+                new DateOnly(2012, 6, 21)
+            )
+
+
+        ];
+        
+        app.MapGet("/games", () => games);
+        app.MapGet("/games/{id}", (int id) => games.Find(g => g.Id == id));
+        
+        
         app.MapGet("/", () => "Hello World!");
 
         app.Run();
